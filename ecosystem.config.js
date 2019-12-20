6dea515a0d8ce211dbdd225c0e08ceb43db5b21b
KZ <me@kez.io>
2019-12-19 23:47:30 -0500

    Add a PM2 config file.

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    apps : [{
+        name: 'Server',
+        script: 'index.js',
+        instances: 1,
+        autorestart: true,
+        watch: true,
+        ignore_watch: [
+            "data/**/*.mp3",
+            "data/**/*.zip",
+            "data/**/*.gz",
+            "data/**/*.lastModified",
+            "data/**/*.html",
+            "data/**/*.json",
+            "data/**/*.csv",
+            "data/**/*.csv",
+            "data/**/*.csv",
+            "**/temp/*",
+            "data/dictionaries/wisdom/*",
+            "data/examples/audio/*",
+            "node_modules/*",
+            ".git/*"
+        ],
+        env: {
+            NODE_ENV: 'development'
+        },
+        env_production: {
+            NODE_ENV: 'production'
+        }
+    }]
+};
