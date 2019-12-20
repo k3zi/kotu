b10980d3617b75009eb0f41402b42940912e4231
KZ <me@kez.io>
2019-12-19 23:54:15 -0500

    Bump up memory limit.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,26 +6,27 @@ module.exports = {
         autorestart: true,
         watch: true,
         ignore_watch: [
-            "data/**/*.mp3",
-            "data/**/*.zip",
-            "data/**/*.gz",
-            "data/**/*.lastModified",
-            "data/**/*.html",
-            "data/**/*.json",
-            "data/**/*.csv",
-            "data/**/*.csv",
-            "data/**/*.csv",
-            "**/temp/*",
-            "data/dictionaries/wisdom/*",
-            "data/examples/audio/*",
-            "node_modules/*",
-            ".git/*"
+            'data/**/*.mp3',
+            'data/**/*.zip',
+            'data/**/*.gz',
+            'data/**/*.lastModified',
+            'data/**/*.html',
+            'data/**/*.json',
+            'data/**/*.csv',
+            'data/**/*.csv',
+            'data/**/*.csv',
+            '**/temp/*',
+            'data/dictionaries/wisdom/*',
+            'data/examples/audio/*',
+            'node_modules/*',
+            '.git/*'
         ],
         env: {
             NODE_ENV: 'development'
         },
         env_production: {
             NODE_ENV: 'production'
-        }
+        },
+        node_args: '--max_old_space_size=16384'
     }]
 };
